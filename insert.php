adba52f653681a2d5907c7f4af64faf5fb976f5f
Satriawan Ridho <satriawanr14@gmail.com>
2017-02-21 11:03:27 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -8,10 +8,22 @@ $data = json_decode(file_get_contents('php://input'));
 
 // cek jika var data tidak kosong
 if($data) {
+// insert database
+$sql = $db->prepare("UPDATE kota SET nama_kota = :nama_kota WHERE id = :id");
+$sql->bindParam(':nama_kota', $data->nama_kota);
+$sql->bindParam(':id', $data->id);
+$sql->execute();
+    
+}
 
+// tampung data dari form input
+$data = json_decode(file_get_contents('php://input'));
+
+// cek jika var data tidak kosong
+if($data) {
 // insert database
 $sql = $db->prepare("INSERT INTO kota VALUES('', :nama_kota)");
 $sql->bindParam(':nama_kota', $data->nama_kota);
-$sql->execute();
+$sql->execute();    
 
 }
